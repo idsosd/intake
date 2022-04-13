c85fa89b001e00ca0677cb061bd82994fc8813d1
Ids Osinga <ip.osinga@alfa-college.nl>
2022-04-13 13:53:54 +0200

    v 2022-04-13 13:53

--- gesprek.class.php
+++ gesprek.class.php
@@ -340,7 +340,7 @@ class Gesprek
             $query -> bindParam(':zorgstatus', $_POST['zorgstatus']);
             $uitkomst = $_POST['uitkomst'];
             if($_POST['uitkomst'] == '')
-                $uitkomst = 7
+                $uitkomst = 7;
             $query -> bindParam(':uitkomst', $uitkomst);
             $query -> bindParam(':id', $_POST['gesprid']);
             $query -> execute();
