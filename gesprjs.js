078bd0667601f6e0c4b597aa2d846a0f48fe5617
Ids Osinga <ip.osinga@alfa-college.nl>
2022-06-21 11:48:27 +0200

    v 2022-06-21 11:48

--- gesprjs.js
+++ gesprjs.js
@@ -93,23 +93,23 @@ function addGesprek()
 {
 	var form="<form id='addgesprek' onsubmit=\"insertGesprek(); return false\">" +
 				"<div class='row'>" +
-		"<div class='col'>" +
+		"<div class='col-1'>" +
 		"<label for='studnr'><b>Studentnr</b></label>" +
 		"<input id='studnr' class='form-control' type='text'>" +
 		"</div>" +
-				"<div class='col'>" +
+				"<div class='col-4'>" +
 				"<label for='achternaam'><b>Achternaam</b></label>" +
 				"<input id='achternaam' class='form-control' type='text' placeholder='achternaam' required>" +
 				"</div>" +
-				"<div class='col'>" +
+				"<div class='col-1'>" +
 				"<label for='voorv'><b>Voorv.</b></label>" +
 				"<input id='voorv' class='form-control' type='text' placeholder='voorvoegsel'>" +
 				"</div>" +
-				"<div class='col'>" +
+				"<div class='col-4'>" +
 				"<label for='roepnaam'><b>Roepnaam</b></label>" +
 				"<input id='roepnaam' class='form-control' type='text' placeholder='roepnaam' required>" +
 				"</div>" +
-		"<div class='col'>" +
+		"<div class='col-2'>" +
 		"<label for='geslacht'><b>Geslacht</b></label>" +
 		"<SELECT id='geslacht' class='form-control' required>" +
 		"<option value=''>kies...</option>" +
